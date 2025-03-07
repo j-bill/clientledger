1c9f756b7e0004a16d6d45d4a6218fdf0f55f0dc
Julian Billinger <julian.billinger@admin-box.de>
2025-03-07 18:08:33 +0100

    basic favicon.ico

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel with Vite and Vue</title>
+    <!-- favicon.png -->
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
+    <title>Clientledger</title>
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
 
