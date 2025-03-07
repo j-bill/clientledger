703651cab8f65d0bf734298ac9257ba70793f1ee
Julian Billinger <julian.billinger@admin-box.de>
2025-03-07 18:04:20 +0100

    refactor: replace Tailwind CSS with SCSS for styling and update Vite configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel with Vite and Vue</title>
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
